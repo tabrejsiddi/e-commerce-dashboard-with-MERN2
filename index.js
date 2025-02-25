ad42806258d25006d44b54851c7b6906559e2256
tabrejsiddi <tabrejsiddi5@gmail.com>
2025-02-26 00:36:24 +0530

    production mode

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const Jwt = require("jsonwebtoken");
 require("dotenv").config();
+const path = require('path');
 const jwtKey = process.env.JWT_KEY;
-const path = require("path");
-
+const _dirname = path.resolve();
 // Importing the database configuration & user model
 require("./Database/config");
 const Users = require("./Database/Users");
@@ -126,12 +126,11 @@ function verifyToken(req, res, next) {
     res.status(401).send({ result: "Please add token with header" });
   }
 }
-
-app.get("/",(req,res)=>{
-  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
-  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+app.use(express.static(path.join(_dirname, "frontend/build")));
+console.log(path.resolve(_dirname, "frontend", "build"));
+app.get("*",(req,res)=>{ 
+  res.sendFile(path.resolve(_dirname, "frontend", "build", "index.html"));
 });
-
 const PORT =process.env.PORT || 5000;
 //Middlewares
 app.listen(PORT, () => {
